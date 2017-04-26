1d3129f8c7ef0dbcb50cebdcde1e29f613f1a1cc
Roberto Ortega <berto.ort@gmail.com>
2017-04-26 15:48:15 -0600

    🎉 Initial commit

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour {
+//
+//	[Serializable]
+//	public class Count {
+//		public int minimum;
+//		public int maximum;
+//
+//		public Count (int min, int max) {
+//			minimum = min;
+//			maximum = max;
+//		}
+//	}
+//
+//	public int columns = 8;
+//	public int rows = 8;
+//	public Count wallCount = new Count (5, 9);
+//	public Count foodCount = new Count (1, 5);
+//	public GameObject exit;
+//	public GameObject[] floorTiles;
+//	public GameObject[] wallTiles;
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+//
+//	public BoardManager boardscript;
+//	private int level = 0;
+//
+//	void Awake () {
+//		boardscript = GetComponent<BoardManager> ();
+//		InitGame ();
+//	}
+//
+//	void InitGame() {
+//		boardscript.SetupScene (level);
+//	}
+//	
+//	// Update is called once per frame
+//	void Update () {
+//		
+//	}
+}
