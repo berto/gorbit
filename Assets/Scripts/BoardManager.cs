0a1ac18e5d3b9a9c68ff3850548fa31293c60145
Roberto Ortega <berto.ort@gmail.com>
2017-04-26 22:59:36 -0600

    ✨ Add grid to asteroids

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -1,25 +1,63 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class BoardManager : MonoBehaviour {
-//
-//	[Serializable]
-//	public class Count {
-//		public int minimum;
-//		public int maximum;
-//
-//		public Count (int min, int max) {
-//			minimum = min;
-//			maximum = max;
-//		}
-//	}
-//
-//	public int columns = 8;
-//	public int rows = 8;
-//	public Count wallCount = new Count (5, 9);
-//	public Count foodCount = new Count (1, 5);
-//	public GameObject exit;
-//	public GameObject[] floorTiles;
-//	public GameObject[] wallTiles;
+
+	// boardSizes s,m,l,xl. Defaults to small
+	public string boardSize;
+	public GameObject[] asteroids;
+	public GameObject background;
+	private Board board;
+	private List <Vector3> gridPositions = new List <Vector3> ();
+	private float gridX;
+	private float gridY;
+	private float boardX;
+	private float boardY;
+
+	private class Board {
+		public int columns = 7;
+		public int rows = 4;
+		private string[] sizes = new string[4] {"s", "m", "l", "xl"};
+
+		public Board (string size) {
+			for (int i = 1; i < sizes.Length + 1; i++) {
+				if (sizes[i-1] == size) {
+					columns *= i;
+					rows *= i;
+				}
+			}
+		}
+	}
+
+	void BoardSetup (int level) {		
+		board = new Board (boardSize);
+		Instantiate (background);
+		boardX = background.GetComponent<Renderer> ().bounds.size.x;
+		boardY = background.GetComponent<Renderer> ().bounds.size.y;
+		gridX = boardX / board.columns;
+		gridY = boardY / board.rows;
+	}
+
+	void DrawBoard () {
+		for (int x = 0; x < board.columns; x++) {
+			for (int y = 0; y < board.rows; y++) {
+				GameObject asteroid = asteroids[Random.Range (0,4)];
+				asteroid.transform.localScale = new Vector3 (1, 1, 0f);
+				float asteroidSizeX = asteroid.GetComponent<Renderer> ().bounds.size.x;
+				float asteroidSizeY = asteroid.GetComponent<Renderer> ().bounds.size.y;
+				asteroid.transform.localScale = new Vector3 (gridX/asteroidSizeX, gridY/asteroidSizeY, 0f);
+				float positionY = y * gridY - (boardY/2) + gridY/2;
+				float positionX = x * gridX - (boardX/2) + gridX/2;
+				Vector3 position = new Vector3 (positionX, positionY, 0f);
+				Instantiate (asteroid, position, Quaternion.identity);
+			}
+		}
+	}
+
+	public void SetupScene (int level) {
+		BoardSetup (level);
+		DrawBoard ();
+	}
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,22 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour {
-//
-//	public BoardManager boardscript;
-//	private int level = 0;
-//
-//	void Awake () {
-//		boardscript = GetComponent<BoardManager> ();
-//		InitGame ();
-//	}
-//
-//	void InitGame() {
-//		boardscript.SetupScene (level);
-//	}
-//	
-//	// Update is called once per frame
-//	void Update () {
-//		
-//	}
+public class GameManager: MonoBehaviour {
+
+	//Static instance of GameManager which allows it to be accessed by any other script.
+	public static GameManager instance = null;              
+	public BoardManager boardscript;
+	private int level = 0;
+
+	void Awake () {
+		//Check if instance already exists
+		if (instance == null)
+			instance = this;
+		else if (instance != this)
+			Destroy(gameObject);    
+		DontDestroyOnLoad(gameObject);
+
+		boardscript = GetComponent<BoardManager> ();
+		InitGame ();
+	}
+
+	void InitGame() {
+		boardscript.SetupScene (level);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
 }
