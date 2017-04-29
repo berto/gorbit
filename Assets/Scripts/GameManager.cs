38ae2b728ad2e19d7ebdfc804e61f71898551adb
Roberto Ortega <berto.ort@gmail.com>
2017-04-29 12:22:28 -0600

    ✨ Pulsing restart text displays and player is limited by bounds

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,8 +9,11 @@ public class GameManager: MonoBehaviour {
 	//Static instance of GameManager which allows it to be accessed by any other script.
 	public static GameManager instance = null;
 	private Text levelText;
+	private Text restartText;
 	public BoardManager boardscript;
 	public int level = 0;
+	private float maxX = 100;
+	private float maxY = 100;
 
 	void Awake () {
 		//Check if instance already exists
@@ -32,8 +35,20 @@ public class GameManager: MonoBehaviour {
 
 	void InitGame() {
 		levelText = GameObject.Find("LevelText").GetComponent<Text>();
+		restartText = GameObject.Find("RestartText").GetComponent<Text>();
+		restartText.enabled = false;
 		levelText.text = "Level: " + level.ToString();
 		boardscript.SetupScene (level);
+	}
+
+	void Update () {
+		float vertExtent = Camera.main.orthographicSize;    
+		float horzExtent = vertExtent * Screen.width / Screen.height;
+		GameObject[] players = GameObject.FindGameObjectsWithTag ("Player");
+		for (int i = 0; i < players.Length; i++) {
+			displayRestartText (players [i].transform.position.x, players [i].transform.position.y, horzExtent, vertExtent);
+			limitPlayer (players[i], players [i].transform.position.x, players [i].transform.position.y);
+		}
 	}
 
 	public void NextLevel() {
@@ -59,9 +74,27 @@ public class GameManager: MonoBehaviour {
 	public void Reset() {
 		boardscript.Reset ();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	void displayRestartText(float playerX, float playerY, float cameraX, float cameraY) {
+		if (playerX < -cameraX || playerX > cameraX) {
+			restartText.enabled = true;
+		} else if (playerY < -cameraY || playerY > cameraY) {
+			restartText.enabled = true;
+		} else {
+			restartText.enabled = false;
+		}
 	}
+
+	void limitPlayer(GameObject player, float playerX, float playerY) {
+		if (playerX < -maxX) {
+			player.transform.position = new Vector3 (-maxX, player.transform.position.y, 0f);
+		} else if (playerX > maxX) {
+			player.transform.position = new Vector3 (maxX, player.transform.position.y, 0f);
+		} else if (playerY > maxY) {
+			player.transform.position = new Vector3 (player.transform.position.y, maxY, 0f);
+		} else if (playerY < -maxY) {
+			player.transform.position = new Vector3 (player.transform.position.y, -maxY, 0f);
+		}
+	}
+
 }
---
+++ Assets/Scripts/Pulsing.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pulsing : MonoBehaviour {
+
+	private Text text;
+	private Color colorT = Color.red;
+
+	void Update () {
+		text = gameObject.GetComponent<Text> ();
+		text.color = new Color(text.color.r,text.color.g,text.color.b, Mathf.PingPong(Time.time, 2.0f));
+	}
+		
+}
